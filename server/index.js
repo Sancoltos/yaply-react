88cc70665070d4786746d21cff0800cfafc5b51e
ADAM_WALTERS <adamwalters59@yahoo.com>
2025-04-14 18:36:39 -0400

    server updates jeez
    change the port to not listen on localhost and instead 0.0.0.0 which should work for cloud solutions

--- server/index.js
+++ server/index.js
@@ -182,6 +182,6 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
